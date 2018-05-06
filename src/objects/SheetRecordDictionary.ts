7d3270a4bc73b863a484e9f3d84a5b2a218c9ea9
kevincar <kevincarrolldavis@gmail.com>
2018-05-06 09:06:09 -0600

    updated SheetRecordDictionary

--- src/objects/SheetRecordDictionary.ts
+++ src/objects/SheetRecordDictionary.ts
@@ -9,34 +9,34 @@
 
 class SheetRecordDictionary extends SheetObjectDictionary<TRRecord> {
 
-	//constructor(sheet: Sheet) {
-		//super(Member, sheet);
-	//}
+	constructor(sheet: Sheet) {
+		super(TRRecord, sheet);
+	}
 
-	//translate(): Member[] {
-		//if(this.sheet == null) throw `Cannot translate a null sheet`;
+	translate(): TRRecord[] {
+		if(this.sheet == null) throw `Cannot translate a null sheet`;
 
-		//// Get Values for all columns expect those columns that are formula dependant
-		//let members: Member[] = [];
-		//this.sheet.values.forEach((rowData: any[], rowIndex: number): void => {
-			//if(this.sheet == null) throw `sheet is null and should not be!`;
-			//let headers: string[] = this.sheet.headers;
+		// Get Values for all columns expect those columns that are formula dependant
+		let records: TRRecord[] = [];
+		this.sheet.values.forEach((rowData: any[], rowIndex: number): void => {
+			if(this.sheet == null) throw `sheet is null and should not be!`;
+			let headers: string[] = this.sheet.headers;
 
-			//let data: SheetObjectInterface = {};
-			//headers.forEach((header: string, headerIndex: number): void => {
-				//data[header] = rowData[headerIndex];
-				//if(header = "Expiring In (mo)") {
-					//if(this.sheet == null) throw `SheetMemberDictionary::translate - sheet is null`;
-					//data[header] = this.sheet.formulas[rowIndex][headerIndex];
-				//}
-			//});
+			let data: SheetObjectInterface = {};
+			headers.forEach((header: string, headerIndex: number): void => {
+				data[header] = rowData[headerIndex];
+				if(header = "Expiring In (mo)") {
+					if(this.sheet == null) throw `SheetRecordDictionary::translate - sheet is null`;
+					data[header] = this.sheet.formulas[rowIndex][headerIndex];
+				}
+			});
 
-			//if(this.ctor == null) throw `SheetMemberDictionary::translate - constructor is null!`;
-			//let instance: Member = new this.ctor(data);
-			//if(!instance.validate(data)) return;
-			//members.push(instance);
-		//});
+			if(this.ctor == null) throw `SheetRecord Dictionary::translate - constructor is null!`;
+			let instance: TRRecord = new this.ctor(data);
+			if(!instance.validate(data)) return;
+			records.push(instance);
+		});
 
-		//return members;
-	//}
+		return records;
+	}
 }
